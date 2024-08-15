b26412fecc0cc744ae6c09ba2dd1dedf2ade4fdf
xueyulinn <xueyulin2001@163.com>
2024-08-15 08:29:15 +0800

    add player jump functions

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player:
         self.height = 40
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = (x, y)
+        self.velY = 0
         self.flip = False
 
     def move(self):
@@ -16,6 +17,7 @@ class Player:
         dy = 0
         key = pygame.key.get_pressed()
 
+        # process keypresses
         if key[pygame.K_a]:
             dx = -10
             self.flip = True
@@ -24,12 +26,20 @@ class Player:
             dx = 10
             self.flip = False
 
+        self.velY += settings.GRAVITY
+        dy += self.velY
+
         if self.rect.left + dx < 0:
             dx = -self.rect.left
         elif self.rect.right + dx > settings.WINDOW_WIDTH:
             dx = settings.WINDOW_WIDTH - self.rect.right
 
+        if self.rect.bottom + dy > settings.WINDOW_HEIGHT:
+            dy = 0
+            self.velY = -20
+
         self.rect.left += dx
+        self.rect.bottom += dy
 
     def draw(self, screen):
         screen.blit(pygame.transform.flip(self.image, self.flip, False),
